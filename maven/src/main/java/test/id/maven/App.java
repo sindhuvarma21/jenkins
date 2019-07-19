56270029b2ecf5bccf60a3bfa094814b82ff60f3
sindhu <sindhuvemulamandha@gmail.com>
2019-07-19 11:34:20 -0400

    first commit

---
+++ maven/src/main/java/test/id/maven/App.java
@@ -0,0 +1,13 @@
+package test.id.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
